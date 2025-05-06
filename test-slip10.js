873dd1d71b9d1147957048158df8d95932dfcc8a
KraakeAA <136975329+KraakeAA@users.noreply.github.com>
2025-05-06 12:03:19 +0100

    Update test-slip10.js

--- test-slip10.js
+++ test-slip10.js
@@ -1,11 +1,1 @@
-import { mnemonicToSeedSync } from 'bip39';
-import { SLIP10Node } from '@metamask/key-tree';
 
-const mnemonic = 'abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about';
-const seedBytes = new Uint8Array(mnemonicToSeedSync(mnemonic)); // use sync version for simplicity
-
-(async () => {
-  const root = await SLIP10Node.fromSeed(seedBytes, { curve: 'ed25519' });
-  const child = await root.derive("slip10:m/44'/501'/0'/0'/0'");
-  console.log('Public Key Bytes:', child.publicKeyBytes);
-})();
